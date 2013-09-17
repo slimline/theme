5ffd4982afc72d0ee08f2fd4cfcc29d5daaf65b6
Michael Dozark <michael@offthewallmedia.com>
2013-09-17 13:01:36 -0700

    convert pply_filters to slimline_apply_filters, backtick filter names in PHPDoc descriptions.

--- inc/conditionals.php
+++ inc/conditionals.php
@@ -55,7 +55,7 @@ function slimline_conditional_safe( $function_name ) {
  *
  * Checks to see if we are on a blog page; either the blog home or an archive. Can
  * also include single posts if the check_single_posts parameter is true. Developers
- * can modify the returned boolean using the slimline_is_blog filter.
+ * can modify the returned boolean using the `slimline_is_blog` filter.
  *
  * @param bool $check_single_posts Whether or not to count single posts as a blog page. Default is false.
  * @return bool Whether or not the user is viewing a blog page.
@@ -63,7 +63,7 @@ function slimline_conditional_safe( $function_name ) {
  */
 function slimline_is_blog( $check_single_posts = false ) {
 
-	return apply_filters( 'slimline_is_blog', ( 'post' == get_post_type() && ( is_home() || is_archive || ( $check_single_posts && is_single() ) ) ), $check_single_posts );
+	return slimline_apply_filters( 'slimline_is_blog', ( 'post' == get_post_type() && ( is_home() || is_archive || ( $check_single_posts && is_single() ) ) ), $check_single_posts );
 }
 
 /**
@@ -92,7 +92,7 @@ function slimline_or( $function_names ) {
  * This conditional determines whether or not post content is meant to be displayed
  * on the current post. By default it returns true on single posts and on the first
  * page of the blog home. Developers can modify the returned boolean using the 
- * slimline_show_content filter.
+ * `slimline_show_content` filter.
  *
  * @global obj $slimline The Slimline theme object.
  * @return bool Whether or not to show the content
@@ -106,7 +106,7 @@ function slimline_show_content() {
 	 * or index pages without checking each individual post.
 	 */
 	if ( ! isset( $slimline->show_content ) )
-		$slimline->show_content = apply_filters( 'slimline_show_content', ( is_single() || ( is_home() && ! is_paged() ) ) );
+		$slimline->show_content = slimline_apply_filters( 'slimline_show_content', ( is_single() || ( is_home() && ! is_paged() ) ) );
 
 	return $slimline->show_content;
 }
@@ -117,7 +117,7 @@ function slimline_show_content() {
  * This conditional determines whether or not a post excerpt is meant to be shown
  * on the current post. By default it returns true on archive pages or on single posts
  * where a custom excerpt is present. Developers can modify the returned boolean
- * using the slimline_show_excerpt filter.
+ * using the `slimline_show_excerpt` filter.
  *
  * @global obj $slimline The Slimline theme object.
  * @return bool Whether or not to show the excerpt
@@ -131,7 +131,7 @@ function slimline_show_excerpt() {
 	 * or index pages without checking each individual post.
 	 */
 	if ( ! isset( $slimline->show_excerpt ) )
-		$slimline->show_excerpt = apply_filters( 'slimline_show_excerpt', ( is_archive() || is_paged() || ( is_single() && has_excerpt() ) ) );
+		$slimline->show_excerpt = slimline_apply_filters( 'slimline_show_excerpt', ( is_archive() || is_paged() || ( is_single() && has_excerpt() ) ) );
 
 	return $slimline->show_excerpt;
 }
