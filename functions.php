6ae400c826d2a3bf6a6551afbdb233715a08a02d
Michael Dozark <michael@offthewallmedia.com>
2013-09-17 19:45:06 -0700

    start functions.php

---
+++ functions.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * The functions file is used to initialize everything in the theme.  It controls how the theme is loaded and 
+ * sets up the supported features, default actions, and default filters.  If making customizations, users 
+ * should create a child theme and make changes to its functions.php file.
+ * 
+ * Function names begin with the prefix "slimline" per the WordPress Theme Repository guidelines
+ * {@see http://make.wordpress.org/themes/guidelines/guidelines-recommended/}. Theme functions are named 
+ * according to the following decision tree:
+ * 1. Is this an initialization function for a core Slimline module?
+ *    Yes: slimline_{module name} (ex: `slimine_admin`)
+ *    No : Continue.
+ * 2. Is the function hooked to a core WordPress action?
+ *    Yes: Does it ONLY fire `slimline_do_action()`?
+ *         Yes: slimline_{WordPress action name} (ex: `slimline_wp_head`}
+ *         No : Continue.
+ *    No : Continue.
+ * 3. Is the function hooked to a core WordPress filter?
+ *    Yes: Does it ONLY filter the content through `slimline_apply_filters()`?
+ *         Yes: slimline_{WordPress filter name} (ex: `slimline_body_class`}
+ *         No : slimline_{descriptive name}_{WordPress filter name} (ex: `slimline_post_ancestors_body_class`}
+ *    No : Continue.
+ * 4. Does the function ONLY call one core WordPress function one or more times (note that if both a single and
+ *    a plural form of a function exists -- such as register_sidebar() and register_sidebars() -- they are 
+ *    counted as the same function for this purpose)?
+ *    Yes: slimline_{WordPress function name [plural version if applicable]} (ex: `slimline_register_sidebars`)
+ *    No: Continue.
+ * 5. Is the function part of a family of similar functions differentiated only by context or location?
+ *    Yes: slimline_{context/location}_{descriptive name} (ex: `slimline_body_attributes`, `slimline_post_attributes`)
+ *    No : slimline_{descriptive name} (ex: `slimline_show_content`)
+ * 
+ * After the initial theme setup function, functions are listed in alphabetic order.
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU 
+ * General Public License version 2.0, as published by the Free Software Foundation.  You may NOT assume 
+ * that you can use any other version of the GPL.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if not, write 
+ * to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ * @package Slimline
+ * @subpackage Functions
+ * @version 0.0.0
+ * @author Michael Dozark <michael@michaeldozark.com>
+ * @copyright Copyright (c) 2013, Michael Dozark
+ * @link http://www.michaeldozark.com/wordpress/themes/slimline
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ */
+
+/*
+ * Call initialization function. This should be the only instance of add_action that
+ * is not contained within a defined function.
+ */ 
+add_action( 'after_setup_theme', 'slimline_core' );
+
+/**
+ * Initializes the theme like so:
+ * 1. Define globals and constants
+ * 2. Include Slimline core files
+ * 3. Remove unwanted default and/or plugin-added actions
+ * 4. Remove unwanted default and/or plugin-added filters
+ * 5. Add custom actions and action assignments
+ * 6. Add custom filters and filter assignments
+ * 7. Add theme support
+ * 8. Miscellaneous and/or additions dependent on the above, such as adding image sizes
+ *
+ * @global int $content_width Defines the maximum width of embedded media {@see http://codex.wordpress.org/Content_Width}
+ * @global object $slimline The Slimline theme object. Used for storing dynamic variables used in functions and template tags
+ */
+function slimline_core() {
+	global $content_width, $slimline;
+
+	/* 1. Define globals and constants */
+	$content_width = slimline_apply_filters( 'slimline_content_width', 980 );
+	$slimline = new stdClass;
+	define( 'SLIMLINE_DIR', get_template_directory() );
+	define( 'SLIMLINE_INC', trailingslashit( SLIMLINE_DIR ) . 'inc' );
+	define( 'SLIMLINE_URI', get_template_directory_uri() );
+	define( 'SLIMLINE_CSS', trailingslashit( SLIMLINE_URI ) . 'css' );
+	define( 'SLIMLINE_IMG', trailingslashit( SLIMLINE_URI ) . 'img' );
+	define( 'SLIMLINE_JS', trailingslashit( SLIMLINE_URI ) . 'js' );
+
+	/* 2. Include Slimline core files */
+	require( trailingslashit( SLIMLINE_INC ) . 'conditionals.php' );
+	require( trailingslashit( SLIMLINE_INC ) . 'context.php' );
+	require( trailingslashit( SLIMLINE_INC ) . 'post-template.php' );
+	require( trailingslashit( SLIMLINE_INC ) . 'template-tags.php' );
+
+	/* 3. Remove unwanted default and/or plugin-added actions */
+	remove_action( 'wp_head', 'wp_generator' ); // don't show WordPress version number
+	remove_action( 'wp_head', 'pods_meta' ); // don't show version number for Pods Framework
+
+	/* 4. Remove unwanted default and/or plugin-added filters */
+
+	/* 5. Add custom actions and action assignments */
+	add_action( 'wp_enqueue_scripts', 'slimline_wp_enqueue_scripts', 0 ); // fire context-aware hook | inc/default-actions.php
+	add_action( 'wp_head', 'slimline_wp_head', 0 ); // fire context-aware hook | inc/default-actions.php
+	add_action( 'wp_footer', 'slimline_wp_footer', 0 ); // fire context-aware hook | inc/default-actions.php
+
+	/* 6. Add custom filters and filter assignments */
+	add_filter( 'body_class', 'slimline_body_class', 999 ); // add context-aware filtering for body classes | inc/default-filters.php
+	add_filter( 'comment_class', 'slimline_comment_class', 999 ); // add context-aware filtering for comment classes | inc/default-filters.php
+	add_filter( 'post_class', 'slimline_post_class', 999 ); // add context-aware filtering for post classes | inc/default-filters.php
+	add_filter( 'slimline_content', 'do_shortcode' ); // add shortcode awareness for alternative filter to the_content
+	add_filter( 'slimline_content', 'wpautop' ); // add automatic paragraphs for alternative filter to the_content
+	add_filter( 'slimline_content', 'wptexturize' ); // add character substitution for alternative filter to the_content
+	add_filter( 'term_description', 'do_shortcode' ); // make term descriptions shortcode-aware
+	add_filter( 'widget_text', 'do_shortcode' ); // make text widget shortcode-aware
+
+	/* 7. Add theme support */
+
+	/* 8. Miscellaneous and/or additions dependent on the above, such as adding image sizes */
+
+}
