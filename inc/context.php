d9892a65de5efba3c0f3fbd3d1c90ed24f78a3f1
Michael Dozark <michael@michaeldozark.com>
2013-09-17 23:24:11 -0700

    add slimline_add_context_action and slimline_add_context_filter

--- inc/context.php
+++ inc/context.php
@@ -12,6 +12,53 @@
 if ( ! defined( 'ABSPATH' ) ) exit; // exit if accessed directly
 
 /**
+ * slimline_add_context_action action
+ *
+ * Fires a context-specific action based on the action it is hooked to. Primarily intended to
+ * hook to default WordPress action hooks, but can be used on theme- or plugin-specific hooks
+ * as well.
+ *
+ * Should be hooked at priority 0 to allow context-specific actions to modify the base action;
+ * i.e. `add_action( '{base_action}', 'slimline_add_context_action', 0 );`
+ *
+ * @param mixed $args Any number of arguments passed to the action
+ * @since 0.1.0
+ */
+function slimline_add_context_action( $args = '' ) {
+
+	$args = func_get_args(); // get any passed arguments
+
+	$hook = current_filter(); // get current action name {@see http://codex.wordpress.org/Function_Reference/current_filter}
+
+	slimline_do_action( "slimline_{$hook}", $args );
+}
+
+/**
+ * slimline_add_context_filter filter
+ *
+ * Applies a context-specific filter based on the filter it is hooked to. Primarily intended to
+ * hook to default WordPress filter hooks, but can be used on theme- or plugin-specific hooks
+ * as well.
+ *
+ * Should be hooked at priority 999 to allow context-specific filters to modify the base filter
+ * output; i.e. `add_filter( '{base_filter}', 'slimline_add_context_filter', 999 );`
+ *
+ * @param mixed $value The value the to be filtered
+ * @param mixed $args Any number of additional arguments passed through the filter
+ * @since 0.1.0
+ */
+function slimline_add_context_filter( $value = '', $args = '' ) {
+
+	$args = func_get_args(); // get any passed arguments
+
+	$args = array_splice( $args, 0, 1 ); // remove $value from $args array
+
+	$hook = current_filter(); // get current action name {@see http://codex.wordpress.org/Function_Reference/current_filter}
+
+	return slimline_apply_filters( "slimline_{$hook}", $value, $args );
+}
+
+/**
  * slimline_apply_filters function
  *
  * Allows theme developers to apply context-specific filters without having to run conditional
@@ -27,12 +74,12 @@ if ( ! defined( 'ABSPATH' ) ) exit; // exit if accessed directly
  * @param mixed $value The value which the filters hooked to $tag may modify.
  * @param mixed $args All additional arguments for the filter.
  * @return mixed The result after all filter hooks are applied to $value
- * @since 0.0.0
+ * @since 0.1.0
  */
 function slimline_apply_filters( $tag, $value = '', $args = '' ) {
 	global $slimline;
 
-	// retrieve the action arguments, minus the $tag
+	// retrieve the filter arguments, minus the $tag
 	$args = func_get_args();
 	$args = array_splice( $args, 0, 1 );
 
@@ -64,7 +111,7 @@ function slimline_apply_filters( $tag, $value = '', $args = '' ) {
  * @global obj $slimline The Slimline theme object.
  * @param string $tag The base name of the action hook.
  * @param mixed $args All additional arguments for the action.
- * @since 0.0.0
+ * @since 0.1.0
  */
 function slimline_do_action( $tag, $args = '' ) {
 	global $slimline;
@@ -73,8 +120,11 @@ function slimline_do_action( $tag, $args = '' ) {
 	$args = func_get_args();
 	$args = array_splice( $args, 0, 1 );
 
+	// put context array in order from most specific to most generic
+	$contexts = array_reverse( $slimline->context );
+
 	// do context-specific actions first
-	foreach ( $slimline->context as $context )
+	foreach ( $contexts as $context )
 		do_action_ref_array( "{$tag}-{$context}", $args );
 
 	do_action_ref_array( $tag, $args ); // do the generic action last
@@ -86,7 +136,7 @@ function slimline_do_action( $tag, $args = '' ) {
  * Retrieves the current context and stores it in the $slimline->context property.
  *
  * @global obj $slimline The Slimline theme object
- * @since 0.0.0
+ * @since 0.1.0
  */
 function slimline_get_context() {
 	global $slimline;
