4863fee4076de741040962a3922acd0e47552a86
Michael Dozark <michael@offthewallmedia.com>
2013-09-30 20:10:43 -0700

    add IE script enqueue functions

--- inc/script-loader.php
+++ inc/script-loader.php
@@ -9,6 +9,34 @@
  */
 
 /**
+ * slimline_ie_enqueue_script function
+ *
+ * Enqueues a script to be used with IE conditional statements. Since there is not a script_loader_tag filter
+ * we can't truly enqueue the script and instead have to set it to output during wp_print_scripts or
+ * wp_print_footer scripts via a custom action.
+ *
+ * @param string $handle Script name
+ * @param string $src Script url
+ * @param array $deps (optional) Array of script names on which this script depends
+ * @param string|bool $ver (optional) Script version (used for cache busting), set to null to disable
+ * @param bool $in_footer (optional) Whether to enqueue the script before </head> or before </body>
+ * @see http://msdn.microsoft.com/en-us/library/ms537512(v=vs.85).aspx
+ * @since 0.1.0
+ * @todo Hook into script_loader_tag when / if WordPress adds one.
+ */
+function slimline_ie_enqueue_script( $handle, $src, $deps = false, $ver = '', $in_footer = false, $ie = false ) {
+
+	if ( ! wp_script_is( $handle, 'registered' ) ) // register the style if it hasn't been already
+		slimline_ie_register_script( $handle, $src, $deps, $ver, $in_footer, $ie );
+
+	$wp_action = ( $in_footer ? 'wp_print_footer_scripts' : 'wp_print_scripts' );
+
+	// add action to header or footer after all other scripts have printed
+	add_action( $wp_action, create_function( '$handle', 'global $wp_scripts; $ready = slimline_ie_handle_script_deps( "' . $handle . '" ); if ( $ready ) { echo "<!--[if ' . $ie . ']>"; $wp_scripts->do_items( "' . $handle . '" ); echo "<![endif]-->"; }' ), 999 );
+	
+}
+
+/**
  * slimline_ie_enqueue_style function
  *
  * Enqueues a style to be used with IE conditional statements.
@@ -20,13 +48,72 @@
  * @param string $media The media for which this stylesheet has been defined.
  * @param string $ie The IE conditional statement such as 'IE 6', 'lt IE 9', etc.
  * @see http://msdn.microsoft.com/en-us/library/ms537512(v=vs.85).aspx
+ * @since 0.1.0
  */
 function slimline_ie_enqueue_style( $handle, $src, $deps = false, $ver = false, $media = 'all', $ie = 'IE' ) {
 
-	if ( ! wp_script_is( $handle, 'registered' ) ) // register the style if it hasn't been already
+	if ( ! wp_style_is( $handle, 'registered' ) ) // register the style if it hasn't been already
 		slimline_ie_register_style( $handle, $src, $deps, $ver, $media, $ie );
 
 	wp_enqueue_style( $handle );
+}
+
+/**
+ * slimline_ie_enqueue_script function
+ *
+ * Registers a script to be used with IE conditional statements.
+ *
+ * @param string $handle Script name
+ * @param string $src Script url
+ * @param array $deps (optional) Array of script names on which this script depends
+ * @param string|bool $ver (optional) Script version (used for cache busting), set to null to disable
+ * @param bool $in_footer (optional) Whether to enqueue the script before </head> or before </body>
+ * @see http://msdn.microsoft.com/en-us/library/ms537512(v=vs.85).aspx
+ * @since 0.1.0
+ * @todo Hook into script_loader_tag when / if WordPress adds one.
+ */
+function slimline_ie_enqueue_script( $handle, $src, $deps = false, $ver = '', $in_footer = false, $ie = false ) {
+
+	wp_register_script( $handle, $src, $deps, $ver, $in_footer );
+}
+
+
+/**
+ * Check to see if IE-enqueued script dependencies have printed or not and print them now if
+ * needed. Necessary since our IE-enqueued scripts cannot be enqueued via wp_enqueue_scripts.
+ *
+ * @global object $wp_scripts Contains all registered scripts {@see http://core.trac.wordpress.org/browser/tags/3.6.1/wp-includes/class.wp-scripts.php}
+ * @param string $handle The script name
+ * @return bool True if the script is registered and all dependencies have been printed, false if not.
+ * @since 0.1.0
+ */
+function slimline_ie_handle_script_deps( $handle ) {
+	global $wp_scripts;
+
+	if ( ! isset( $wp_scripts->registered[ $handle ] ) )
+		return false; // if the script is not registered we can't get the dependencies
+
+	$deps = $wp_scripts->registered[ $handle ]->deps;
+
+	foreach ( $deps as $dep ) {
+
+		/**
+		 * If the dependency has dependencies of its own, handle those first. If they aren't printed
+		 * and can't be, stop evaluating.
+		 */
+		if ( ! ( $ready = slimline_ie_handle_script_deps( $dep ) ) )
+			return false;
+
+		/**
+		 * If the dependency script hasn't already been printed, print it now.
+		 */
+		if ( ! wp_script_is( $dep, 'done' ) )
+			$wp_scripts->do_items( $dep );
+
+	}
+
+	return true; // all dependencies should be printed at this point.
+
 }
 
 /**
@@ -41,8 +128,11 @@ function slimline_ie_enqueue_style( $handle, $src, $deps = false, $ver = false, 
  * @param string $media The media for which this stylesheet has been defined.
  * @param string $ie The IE conditional statement such as 'IE 6', 'lt IE 9', etc.
  * @see http://msdn.microsoft.com/en-us/library/ms537512(v=vs.85).aspx
+ * @since 0.1.0
  */
-function slimline_ie_enqueue_style( $handle, $src, $deps = false, $ver = '', $media = 'all', $ie = 'IE' ) {
+function slimline_ie_register_style( $handle, $src, $deps = false, $ver = '', $media = 'all', $ie = 'IE' ) {
+
+	wp_register_style( $handle, $src, $deps, $ver, $media );
 
 	if ( $ie ) // create a custom filter for this style to output the conditional statements
 		add_filter( "slimline_style_loader_tag-{$handle}", create_function( '$link', 'return "<!--[if ' . $ie . ']>{$link}<![endif]-->\n";' ) );
@@ -56,6 +146,7 @@ function slimline_ie_enqueue_style( $handle, $src, $deps = false, $ver = '', $me
  * @param string $link The <link href="" /> tag from WP_Styles->do_item()
  * @param string $handle The handle for the style chosen at wp_register_style() or wp_enqueue_style()
  * @see http://codex.wordpress.org/Plugin_API/Filter_Reference/style_loader_tag
+ * @since 0.1.0
  */
 function slimline_style_loader_tag( $link, $handle ) {
 
