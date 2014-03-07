4322e6856e300b8deb1e5a34ccf71389f93dc4f6
Michael Dozark <michael@michaeldozark.com>
2014-03-06 18:28:53 -0800

    add hooks locations to comments

--- site-footer.php
+++ site-footer.php
@@ -15,8 +15,8 @@
 			/**
 			 * slimline_site_footer hook
 			 *
-			 * @hook slimline_get_footer_nav - 10 (gets nav-footer template part)
-			 * @hook slimline_get_copyright_notice - 20 (gets notice-copyright template part)
+			 * @hook slimline_get_footer_nav - 10 (gets nav-footer template part) | inc/hooks.php
+			 * @hook slimline_get_copyright_notice - 20 (gets notice-copyright template part) | inc/hooks.php
 			 */
 			do_action( 'slimline_site_footer' );
 		?>
