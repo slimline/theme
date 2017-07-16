8ce5fd3bca728bc62a96406d7ab09a6f150b5707
Michael Dozark <michael@michaeldozark.com>
2017-07-15 20:54:52 -0600

    add site footer

---
+++ parts/footer/footer.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Site Footer
+ *
+ * Contains the site <footer> tag
+ *
+ * @package    Slimline\Theme
+ * @subpackage Template Parts
+ * @since      0.1.0
+ */
+
+/**
+ * exit if accessed directly
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+} // if ( ! defined( 'ABSPATH' ) )
+
+?>
+
+	<footer <?php slimline_footer_attributes(); ?>>
+
+
+
+	</footer><!-- #site-footer -->
