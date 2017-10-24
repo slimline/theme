bc67e2dbc5535d60c0ecf868afc81e810d62cb18
Michael Dozark <michael@michaeldozark.com>
2017-10-24 17:52:15 -0600

    standard loop for archive pages

---
+++ parts/loop.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Main content area
+ *
+ * Contains the site <main> tag
+ *
+ * @package    Slimline\Theme
+ * @subpackage Template Parts
+ * @since      0.3.0
+ */
+
+/**
+ * exit if accessed directly
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+} // if ( ! defined( 'ABSPATH' ) )
+?>
+
+	<div class="index-loop">
+
+		<?php if ( have_posts() ) : ?>
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php slimline_get_template_part( 'parts/content', get_post_type(), get_post_format() ); ?>
+
+			<?php endwhile; // while ( have_posts() ) ?>
+
+			<?php
+				get_template_part( 'parts/nav', 'index' );
+			?>
+
+		<?php else : // if ( have_posts() ) ?>
+
+			<?php
+				get_template_part( 'parts/content', 'none' );
+			?>
+
+		<?php endif; // if ( have_posts() ) ?>
+
+	</div><!-- .index-loop -->
