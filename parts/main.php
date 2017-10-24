34731fad95e5ece0124c98120664bdf8710c29aa
Michael Dozark <michael@michaeldozark.com>
2017-10-24 17:51:06 -0600

    put <main> into separate template part

---
+++ parts/main.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Main content area
+ *
+ * Contains the site <main> tag
+ *
+ * @package    Slimline\Theme
+ * @subpackage Template Parts
+ * @since      0.3.0
+ */
+
+/**
+ * exit if accessed directly
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+} // if ( ! defined( 'ABSPATH' ) )
+?>
+
+	<main <?php slimline_main_attributes(); ?>>
+		<?php
+			/**
+			 * Load the primary theme template (ex. single.php, index.php)
+			 *
+			 * @link https://github.com/slimline/theme/wiki/slimline_get_primary_template()
+			 */
+			slimline_get_primary_template();
+		?>
+	</main>
